86f82d6b5169facb6eef31c68c6845164cf9099e
Rofiqfdilah <121142060+mrofiqfdilah@users.noreply.github.com>
2024-09-06 07:00:09 +0700

    Update AuthController.php

--- REST API/app/Http/Controllers/Api/AuthController.php
+++ REST API/app/Http/Controllers/Api/AuthController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\{Validator};
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
